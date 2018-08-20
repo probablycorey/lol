11e0448499f867bda6e3d6a010710108eec96531
Corey Johnson <probablycorey@github.com>
2018-08-20 10:43:24 -0700

    This is neat!

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
 
-
 app.get('/', (req, res) => {
   console.log("sending another lulz")
-  res.send("lol")
+  res.send("lol coreyz")
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
